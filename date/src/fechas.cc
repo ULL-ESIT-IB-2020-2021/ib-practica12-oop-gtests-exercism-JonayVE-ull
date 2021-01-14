1d16feaf589fe3bddc85ceb3ef9b391264250654
Gabriel Jonay Vera Estévez <alu0101398198@ull.edu.es>
2021-01-14 11:59:28 +0000

    Update fechas.cc

--- date/src/fechas.cc
+++ date/src/fechas.cc
@@ -9,7 +9,7 @@
  * @brief Desarrolle una clase Fecha que permita representar y gestionar fechas. Incorpore en la clase los miembros de datos 
  *        y métodos que considere adecuados para la finalidad que se persigue en este ejercicio. Incluya un método que permita 
  *        determinar si el año correspondiente a una fecha es un año bisiesto o no. Realice un programa cliente fechas.cc que 
- *        tome como parámetro una fecha, un número y un nombre de fichero:
+ *        tome como parámetro un fichero de entrada y un fichero de salida:
  * 
  *
  */
@@ -19,10 +19,16 @@
 
 #include "fechas.h"    
 
+/** Print function
+ *  La función permite imprimir los objetos de la clase Date
+ */
 void Date::Print() {
   std::cout << "The Date is " << day_ << "/" << month_ << "/" << year_ << std::endl;
 }
 
+/** IsLeapYear function
+ *  La función permite saber saber si el año del objeto Date es bisiesto.
+ */
 bool Date::IsLeapYear(){
   if(year_<100){
     if(((year_%100)%4)==0){
@@ -39,6 +45,10 @@ bool Date::IsLeapYear(){
   }
 }
 
+/** Overload < function
+ *  @param[in] d1 Date Object
+ *  @param[in] d2 Date Object
+ */
 bool operator< (const Date &d1, const Date &d2){
   if(d1.year_ < d2.year_){
     return true;
@@ -54,6 +64,10 @@ bool operator< (const Date &d1, const Date &d2){
   return false;
 }
 
+/** Overload > function
+ *  @param[in] d1 Date Object
+ *  @param[in] d2 Date Object
+ */
 bool operator> (const Date &d1, const Date &d2){
   if(d1.year_ > d2.year_){
     return true;
@@ -69,11 +83,19 @@ bool operator> (const Date &d1, const Date &d2){
   return false;
 }
 
+/** Overload << function
+ *  @param[in] d1 Date Object
+ *  @param[in] d2 Date Object
+ */
 std::ostream& operator<< (std::ostream &out, const Date& date) {
  out << date.day_ << "/" << date.month_ << "/" << date.year_; 
  return out; 
 }
 
+/** Overload == function
+ *  @param[in] const d1 Date Object
+ *  @param[in] const d2 Date Object
+ */
 bool operator== (const Date &d1, const Date &d2) {
   return (d1.day_ == d2.day_ && d1.month_ == d2.month_ && d1.year_ == d2.year_);
 }
